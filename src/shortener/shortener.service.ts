29bb239b56f799e607254713a634dd7f1dccf823
Fredrick Oluoch <menty44@gmail.com>
2025-05-06 02:38:53 +0300

    Create short hash

--- src/shortener/shortener.service.ts
+++ src/shortener/shortener.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateShortenerDto } from './dto/create-shortener.dto';
 import { UpdateShortenerDto } from './dto/update-shortener.dto';
+import { randomBytes } from 'crypto';
 
 interface TempData {
   originalUrl: string;
@@ -10,6 +11,10 @@ interface TempData {
 
 @Injectable()
 export class ShortenerService {
+  // Maximum SHORT_URL_LENGTH
+  SHORT_URL_LENGTH = 7;
+  // Maximum URL length
+  MAX_URL_LENGTH = 2048;
   private urlMap: {
     [shortUrl: string]: TempData;
     //  {
@@ -19,8 +24,47 @@ export class ShortenerService {
     // };
   } = {};
 
+  /**
+   * Generates a random short URL.
+   * @returns The short URL.
+   */
+  private generateShortUrl(): string {
+    return randomBytes(Math.ceil(this.SHORT_URL_LENGTH / 2))
+      .toString('hex')
+      .slice(0, this.SHORT_URL_LENGTH);
+  }
+
   async encode(createShortenerDto: CreateShortenerDto) {
-    return createShortenerDto;
+    // Check if the long URL already exists
+    const existingShortUrl = Object.keys(this.urlMap).find(
+      (shortUrl) =>
+        this.urlMap[shortUrl].originalUrl === createShortenerDto.originalUrl,
+    );
+    if (existingShortUrl) {
+      return existingShortUrl; // Return the existing short URL
+    }
+
+    // Generate a unique short URL
+    let shortUrl: string;
+    let attempt = 0;
+    do {
+      if (attempt > 10) {
+        throw new Error('Failed to generate unique short URL');
+      }
+      shortUrl = this.generateShortUrl();
+      if (!this.urlMap[shortUrl]) {
+        break; // Exit loop if short URL is unique
+      }
+      attempt++;
+    } while (true);
+
+    // Create and save the new URL mapping
+    this.urlMap[shortUrl] = {
+      originalUrl: createShortenerDto.originalUrl,
+      visits: 0,
+      createdAt: new Date(),
+    };
+    return { shortUrl };
   }
 
   create(createShortenerDto: CreateShortenerDto) {
